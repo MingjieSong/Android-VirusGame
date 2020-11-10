777e32304b399cb3d03362b9825bcc7ef1838ea7
jsil6899 <jsilverstein68@gmail.com>
2020-11-10 14:45:47 -0500

    One virus now moves around on screen and disappears when clicked. Still need to make sure it's the correct virus for the player's range and also add it to the profile

--- app/src/main/java/com/androidApp/virusGame/UI/GameFragment.java
+++ app/src/main/java/com/androidApp/virusGame/UI/GameFragment.java
@@ -1,9 +1,11 @@
 package com.androidApp.virusGame.UI;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,8 +13,10 @@ import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -27,10 +31,12 @@ import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-
-public class GameFragment extends Fragment   {
+public class GameFragment extends Fragment implements View.OnClickListener  {
 
 
     @Override
@@ -38,12 +44,51 @@ public class GameFragment extends Fragment   {
 
         View v = inflater.inflate(R.layout.fragment_game, container, false);
         Activity activity = getActivity();
-        if(activity!=null) {
-            String virusType = activity.getIntent().getStringExtra("virusName");
-            Toast.makeText( activity,  virusType+ " is coming !", Toast.LENGTH_SHORT).show();
-        }
-
+        //just for testing purposes
+        final ImageButton virusButton=v.findViewById(R.id.virus);
+        virusButton.setOnClickListener(this);
+        startRandomButton(virusButton);
         return v;
+    }
+    public static Point getDisplaySize(@NonNull Context context) {
+        Point point = new Point();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getSize(point);
+        return point;
+    }
+
+    private void setButtonRandomPosition(ImageButton button){
+        int randomX = new Random().nextInt(getDisplaySize(getContext()).x);
+        int randomY = new Random().nextInt(getDisplaySize(getContext()).y);
+        button.setX(randomX);
+        button.setY(randomY);
+    }
+    private void startRandomButton(final ImageButton button) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                setButtonRandomPosition(button);
+            }
+        }, 0, 1000);//Update button every second
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent ;
+        switch(view.getId()){
+            //the virus gets destroyed when it's clicked
+            case R.id.virus:
+                view.setVisibility(View.GONE);
+                break;
+
+        }
+        /* this needs to be called after a successful login
+        intent=new Intent( getActivity(), MaskCheckActivity.class);
+        startActivity(intent); */
+        //just called here so we can trigger onDestroy
+        //getActivity().finish();
+
     }
 
 
