739909701adeb5d3c1a7888c8c9c17334bfae845
Tina Liang <liang.980@osu.edu>
2020-11-30 14:22:36 -0600

    add unit test

---
+++ app/src/test/java/com/androidApp/virusGame/UnitTest.java
@@ -0,0 +1,47 @@
+package com.androidApp.virusGame;
+
+import com.androidApp.virusGame.Model.Player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTest {
+    private String name = "Eugene";
+    private String pw = "1234@";
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void singlePlayerTest(){
+        Player p = new Player(name,pw);
+        String result = p.getName();
+        assertEquals(name,result);
+    }
+
+    @Test
+    public void updatePlayerNameTest(){
+        Player p = new Player(name,pw);
+        String nName = "Jackson";
+        p.setName(nName);
+        assertEquals(nName,p.getName());
+    }
+
+    @Test
+    public void updatePlayerPwTest(){
+        Player p = new Player(name,pw);
+        String nPw = "221.=9";
+        p.setPassword(nPw);
+        assertEquals(nPw,p.getPassword());
+    }
+
+
+
+}
